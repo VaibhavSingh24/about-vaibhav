959a5ff603da4c1381514ab281b7c4bfdb3b636f
VaibhavSingh24 <techmate1999@gmail.com>
2019-12-25 17:37:09 +0530

    Uploaded initial files

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(()=>{
+	$('#ig1').hide().fadeIn(1000)
+	$('#ig2').hide().fadeIn(2000)
+	$('#ig3').hide().fadeIn(3000)
+	$('#ig4').hide().fadeIn(4000)
+	$('#ig5').hide().fadeIn(5000)
+	$('#ig6').hide().fadeIn(6000)
+})
